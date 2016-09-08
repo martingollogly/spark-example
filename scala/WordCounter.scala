4c8801cd41363fc9c7d4106abe0756815569d2d3
zemertz <zemertz@gmail.com>
2016-09-08 14:03:08 +0100

    Update WordCounter.scala

--- scala/WordCounter.scala
+++ scala/WordCounter.scala
@@ -7,11 +7,11 @@ object WordCounter {
   def main(args: Array[String]) {
     val conf = new SparkConf(.setAppName("Word Counter"))
     val sc = new SparkContext(conf)
-    val textFile = c.textFile("file:///<SPARK_HOME>/README.md")
+    val textFile = c.textFile("file:///README.md")
     val tokenizedFileData = textFile.flatMap(line=>line.split(" "))
     val countPrep = tokenizedFileData.map(word=>(word,1))
     val counts = countPrep.reduceByKey((accumValue, newValue)=>accumValue + newValue)
     val sortedCounts = counts.sortBy(kvPair=>kvPair._2, false)
-    sortedCounts.saveAsTexFile("file:///<SOME_OUTPUT_LOCATION>/ReadMeWordCount")
+    sortedCounts.saveAsTexFile("file:///Pluralsight/DataWordCount/ReadMeWordCount")
   }
 }
