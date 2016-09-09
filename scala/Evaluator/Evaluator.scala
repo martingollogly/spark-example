e48c8d8c9b9ab3b5382d82814815779c29b51b93
gollogly <martin.gollogly@scisys.co.uk>
2016-09-09 15:53:34 +0100

    Add Evaluator Example for Big Data

--- scala/Evaluator/Evaluator.scala
+++ scala/Evaluator/Evaluator.scala
@@ -1,27 +1,29 @@
+package main
+
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-import org.apache.hadoop.streaming.StreamXmlRecordReader
-import org.apache.hadoop.mapred.JobConf
-import org.apache.hadoop.io.Text
-import org.apache.hadoop.mapred.FileInputFormat
-
 
 object Evaluator {
-	def main(args: Array[String]) {
-		val conf = new SparkConf().setAppName("Language Evaluator")
-		val sc = new SparkContext(conf)
-		val jobConf = new JobConf()
-		
-		/* Set up Hadoop Configuration for Stream XML Reader*/
-		jobConf.set("stream.recordreader.class",
-			        "org.apache.hadoop.streaming.StreamXmlRecordReader")
-		jobConf.set("stream.recordreader.begin", "<page>")
-		jobConf.set("stream.recordreader.end", "</page>")
-		FileInputFormat.addInputPaths(jobConf, "file:///Data/WikiPages_BigData.xml")
-
-		val wikiDocuments = sc.hadoopRDD(jobConf,
-			classOf[org.apache.hadoop.streaming.StreamInputFormat],
-			classOf[Text], classOf[Text])
-
-	}
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Language Evaluator")
+    val sc = new SparkContext(conf)
+    
+    val wikiDocuments = HadoopWikiRDDGenerator
+                          .createUsing(sc, withPath = "file:///Data/WikiPages_BigData.xml")
+    
+	val rawWikiPages = WikiCleaner.clean(wikiDocuments)
+	
+	val tokenizedWikiData = rawWikiPages.flatMap(wikiText=>wikiText.split("\\W+"))
+	
+	val pertinentWikiData = tokenizedWikiData
+	                        .map(wikiToken => wikiToken.replaceAll("[.|,|'|\"|?|)|(]", "").trim)
+	                        .filter(wikiToken=>wikiToken.length > 2)
+	                                         
+	val wikiDataSortedByLength = pertinentWikiData.distinct
+	         .sortBy(wikiToken=>wikiToken.length, ascending = false)
+	         .sample(withReplacement = false, fraction = .01)
+	         .keyBy(wikiToken=>wikiToken.length)
+	         
+	wikiDataSortedByLength.collect.foreach(println)
+  }
 }
